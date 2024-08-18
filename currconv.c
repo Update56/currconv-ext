6d495257400c02e01986916827e93ed6ef55f1e6
Update56 <heupdate56@gmail.com>
2024-08-19 02:03:10 +0700

    New version lib, return all rates? new name func

--- currconv.c
+++ currconv.c
@@ -5,29 +5,20 @@
 
 PG_MODULE_MAGIC;
 
-PG_FUNCTION_INFO_V1(convert_from_rub);
+PG_FUNCTION_INFO_V1(currency_convert);
 
 Datum
-convert_from_rub(PG_FUNCTION_ARGS)
+currency_convert(PG_FUNCTION_ARGS)
 {
+   text     currency = PG_GETARG_TEXT(0);
    float4   input_currency = PG_GETARG_FLOAT4(0);
-   char	   string[32];
-   float    rates[3];
-
    void *handle;
    handle = dlopen ("/home/up56/currconv-ext/libgetconv.so", RTLD_NOW | RTLD_GLOBAL);
    if (!handle) {
       fputs (dlerror(), stderr);
       exit(1);
    }
-
-   void(*getconv)(float[]) = dlsym(handle, "Getconv");
-   getconv(rates);
-
-   for (short int i = 0; i < 3; i++)
-   {
-      rates[i] = rates[i] * input_currency;
-   }
-   sprintf(string, "%f\n%f\n%f\n", rates[0], rates[1], rates[2]);
-   PG_RETURN_TEXT_P(cstring_to_text(string));
+   char*(*getbody)(char*) = dlsym(handle, "GetBody");
+   char* body = getbody(text_to_cstring(currency));
+   PG_RETURN_TEXT_P(cstring_to_text(body));
 }
